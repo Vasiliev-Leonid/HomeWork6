f559c5c9411ba513f29f1ba7596d8378a53744ab
Vasiliev-Leonid <114797981+Vasiliev-Leonid@users.noreply.github.com>
2022-11-17 11:14:27 +0300

    HomeWork6

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, 
+//сколько чисел больше 0 ввёл пользователь.
+//Console.Write("Input your M numbers (Split): ");
+//int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+//int count = 0;
+//for (int i = 0; i < arr.Length; i++)
+//{
+//    if (arr[i] > 0)
+//    { 
+//        count++;
+//    }
+//}
+//Console.WriteLine($"Number of elements > 0: {count}");
+
+
+//Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+//заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+//значения b1, k1, b2 и k2 задаются пользователем.
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+//void CrossPoint(double k1, double b1, double k2, double b2)
+//{
+//double x = (b1-b2)/(k2-k1);
+//double y = (k2*b1-k1*b2)/(k2-k1);
+//if(k1==k2) 
+//Console.Write("There is no cross point!");
+//else
+//Console.Write($"Cross point is located here : ({x}; {y})");
+//}
+
+//Console.WriteLine("Input b1: ");
+//int b1 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Input k1: ");
+//int k1 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Input b2: ");
+//int b2 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Input k2: ");
+//int k2 = Convert.ToInt32(Console.ReadLine());
+//CrossPoint(k1,b1,k2,b2);
